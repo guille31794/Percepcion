0f8896fc479f03bd880e2b06bab159f185a96eaa
JosewLuis <45396436+JosewLuis@users.noreply.github.com>
2019-06-16 11:43:16 +0200

    Parte Foto completa

---
+++ ParteFoto/takeSquares.m
@@ -0,0 +1,10 @@
+function [im]=takeSquares(imagen,Xcol,Ycol,Xfil,Yfil)
+    figure,imshow(imagen);
+    for i=1:10
+        for j=1:10
+            [xC,yC,xLine,yLine]=makeSquare(Xcol(j:j+1,:),Ycol(j:j+1,:),Xfil(i:i+1,:),Yfil(i:i+1,:));
+            im{i,j}=imcrop(imagen,[xC,yC,xLine,yLine]);
+            rectangle('Position',[xC,yC,xLine,yLine],'EdgeColor','m');
+        end
+    end
+end
